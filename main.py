cad62d869f563bbc2f9a41c88aa7b882e2b7a8cc
mdrief-iastate <mdrief@iastate.edu>
2022-08-29 21:12:57 -0500

    Adding code for random walk model

--- main.py
+++ main.py
@@ -3,6 +3,9 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+import numpy
+import pylab
+import random
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -12,5 +15,35 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+n = 1000000
+
+# creating two array for containing x and y coordinate
+# of size equals to the number of size and filled up with 0's
+x = numpy.zeros(n)
+y = numpy.zeros(n)
+
+# filling the coordinates with random variables
+for i in range(1, n):
+    val = random.randint(1, 4)
+    if val == 1:
+        x[i] = x[i - 1] + 1
+        y[i] = y[i - 1]
+    elif val == 2:
+        x[i] = x[i - 1] - 1
+        y[i] = y[i - 1]
+    elif val == 3:
+        x[i] = x[i - 1]
+        y[i] = y[i - 1] + 1
+    else:
+        x[i] = x[i - 1]
+        y[i] = y[i - 1] - 1
+
+# plotting stuff:
+pylab.title("Random Walk ($n = " + str(n) + "$ steps)")
+pylab.plot(x, y)
+pylab.savefig("rand_walk" + str(n) + ".png", bbox_inches="tight", dpi=600)
+pylab.show()
+
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
